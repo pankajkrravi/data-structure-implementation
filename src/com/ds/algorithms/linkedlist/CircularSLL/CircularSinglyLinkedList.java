9298ea18280fe3c26cac8cce4670a51f9e7ee62a
Pankaj <pankaj.ravi@goomo.com>
2021-04-13 17:08:23 +0530

    Circular Singly LL method implemented

--- src/com/ds/algorithms/linkedlist/CircularSLL/CircularSinglyLinkedList.java
+++ src/com/ds/algorithms/linkedlist/CircularSLL/CircularSinglyLinkedList.java
@@ -1,5 +1,7 @@
 package com.ds.algorithms.linkedlist.CircularSLL;
 
+import java.util.NoSuchElementException;
+
 /**
  * @Author pankaj
  * @create 4/13/21 1:13 PM
@@ -55,9 +57,9 @@ public class CircularSinglyLinkedList {
     public void printCircularSLL()
     {
         // check if Circular SLL pointing to  null
-          if(last !=null)
+          if(last ==null)
         {
-            System.out.println("check if Circular LL Last == null !!!!!!!!!!!!!");
+           // System.out.println("check if Circular LL Already Empty");
             return;
         }
         NodeCircular first=last.next;
@@ -68,4 +70,53 @@ public class CircularSinglyLinkedList {
         }
         System.out.println(first.data+" ");
     }
+    //-----------insert at first
+    public void insertFirst(int data)
+    {
+        NodeCircular tmp=new NodeCircular(data);
+        if(last == null)
+        {
+            last=tmp;
+        }else {
+            tmp.next=last.next;
+        }
+        last.next=tmp;
+        length++;
+    }
+    // ------------------insert Last
+    public void insertLast(int data)
+    {
+        NodeCircular temp=new NodeCircular(data);
+        if(last == null) //isEmpty
+        {
+        last=temp;
+         last.next=last;
+        }else {
+            temp.next=last.next;
+            last.next=temp;
+            last=temp;
+        }
+        length++;
+    }
+    //-----------------remove first ------------------
+    public int removeFirst()
+    {
+        if(isEmpty())
+        {
+            throw new NoSuchElementException("circular Linked List is already Empty  !!!!!!!!!!");
+        }
+        NodeCircular temp=last.next;
+        int result=temp.data;
+        if(last.next == last)//Only one Element is Left
+        {
+            last=null;
+        }else {
+            last.next=temp.next;
+        }
+        //temp.next=null;
+        length--;
+        return result;// return temp;  --> Node
+    }
+    // ===================  Remove Last =======================
+
 }
--- src/com/ds/algorithms/linkedlist/CircularSLL/MainRunnerCircular.java
+++ src/com/ds/algorithms/linkedlist/CircularSLL/MainRunnerCircular.java
@@ -7,7 +7,18 @@ package com.ds.algorithms.linkedlist.CircularSLL;
 public class MainRunnerCircular {
     public static void main(String[] args) {
         CircularSinglyLinkedList circularSinglyLinkedList=new CircularSinglyLinkedList();
-            circularSinglyLinkedList.createCircularSinglyLinkedList();
-            circularSinglyLinkedList.printCircularSLL();
+        //circularSinglyLinkedList.createCircularSinglyLinkedList();
+        /*circularSinglyLinkedList.insertFirst(2);
+        circularSinglyLinkedList.insertFirst(8);"Deleted First: "+
+        circularSinglyLinkedList.insertFirst(100);*/
+        circularSinglyLinkedList.insertLast(1);
+        circularSinglyLinkedList.insertLast(2);
+        circularSinglyLinkedList.insertLast(3);
+        circularSinglyLinkedList.printCircularSLL();
+        System.out.println("======================================================");
+        System.out.println("Deleted First: "+ circularSinglyLinkedList.removeFirst());
+        System.out.println("Deleted First: "+circularSinglyLinkedList.removeFirst());
+        System.out.println("Deleted First: "+circularSinglyLinkedList.removeFirst());
+        circularSinglyLinkedList.printCircularSLL();
     }
 }
