71bd17dd431cb7e80a966bb50b62aadbd7562046
Pankaj <pankaj.ravi@goomo.com>
2021-04-13 14:16:29 +0530

    Circular Singly LL implemented

---
+++ src/com/ds/algorithms/CircularSLL/CircularSinglyLinkedList.java
@@ -0,0 +1,71 @@
+package com.ds.algorithms.CircularSLL;
+
+/**
+ * @Author pankaj
+ * @create 4/13/21 1:13 PM
+ */
+public class CircularSinglyLinkedList {
+        private NodeCircular last;
+        private int length;
+
+    private  class NodeCircular
+    {
+        private NodeCircular next;
+        private int data;
+        NodeCircular(int data)
+        {
+            this.data=data;
+        }
+    }
+     CircularSinglyLinkedList()
+     {
+         last=null;
+         length=0;
+     }
+     // Length()
+    public int length()
+    {
+        return length;
+    }
+    //isEmpty()
+    public boolean isEmpty()
+    {
+        return length==0;
+    }
+   // createVCircularSinglyLinkedList
+    public void createCircularSinglyLinkedList()
+    {
+        NodeCircular nodeFirst=new NodeCircular(1);
+        NodeCircular nodeSecond=new NodeCircular(2);
+        NodeCircular nodeThird=new NodeCircular(3);
+        NodeCircular nodeForth=new NodeCircular(4);
+        NodeCircular nodeFifth=new NodeCircular(5);
+        NodeCircular nodeSixth=new NodeCircular(6);
+        // create Link
+        nodeFirst.next=nodeSecond;
+        nodeSecond.next=nodeThird;
+        nodeThird.next=nodeForth;
+        nodeForth.next=nodeFifth;
+        nodeFifth.next=nodeSixth;
+        nodeSixth.next=nodeFirst;//----------
+        last=nodeSixth;
+    }
+
+    // print Circular SLL
+    public void printCircularSLL()
+    {
+        // check if Circular SLL pointing to  null
+          if(last !=null)
+        {
+            System.out.println("check if Circular LL Last == null !!!!!!!!!!!!!");
+            return;
+        }
+        NodeCircular first=last.next;
+        while (first !=last)
+        {
+            System.out.print(first.data+" ");
+            first=first.next;
+        }
+        System.out.println(first.data+" ");
+    }
+}
---
+++ src/com/ds/algorithms/CircularSLL/MainRunnerCircular.java
@@ -0,0 +1,13 @@
+package com.ds.algorithms.CircularSLL;
+
+/**
+ * @Author pankaj
+ * @create 4/13/21 1:21 PM
+ */
+public class MainRunnerCircular {
+    public static void main(String[] args) {
+        CircularSinglyLinkedList circularSinglyLinkedList=new CircularSinglyLinkedList();
+            circularSinglyLinkedList.createCircularSinglyLinkedList();
+            circularSinglyLinkedList.printCircularSLL();
+    }
+}
